2691a0f87ea20965b637d0ce2fe9285f2d902ca7
Alfaraby Indra <alfarabyindra@gmail.com>
2025-01-31 09:45:02 +0700

    first commit ngoding python

---
+++ Tipe Data/tipe1.py
@@ -0,0 +1,58 @@
+# tipe data python
+
+#1. string
+nama="ALFARABY INDRA"
+print(nama)
+
+#2. Integer
+a=20
+b=40 
+c=a+b
+print(c)
+
+d= c-b
+print(d)
+
+#3. Float
+x=3.14
+y=2.5
+print(x*y)
+
+#4. Boolean
+j = 35
+k = 80
+
+print(j > k)
+
+#5. List
+print([1,2,3,4,5])
+print(["satu", "dua", "tiga"])
+
+#6. Tuple
+print((1,2,3,4,5))
+print(("satu", "dua", "tiga"))
+
+#7. Dictionary
+print({"nama":"Budi", 'umur':20})
+
+#8. Set
+# set dengan nlai campuran
+data = {'a','b','c','d',1,2,3,4}
+print (data)
+ 
+# set tidak menampung nilai yang sama
+z = {1,2,3,4,5,6,1,2,3,4}
+print (z)
+
+"""
+    Reserved Keyword Python
+False	def	    if	        raise
+None	del	    import	    return
+True	elif	in	        try
+and	    else	is	        while
+as	    except	lambda	    with
+assert	finally	nonlocal	yield
+break	for	    not
+class	form	or
+continue	    global	    pass
+"""
